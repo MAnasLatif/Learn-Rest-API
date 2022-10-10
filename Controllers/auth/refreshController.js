7a009ef7f62498149e3511eef4e36e42eacb2d43
M Anas Latif <m.anaslatif.com@gmail.com>
2022-10-11 00:09:22 +0500

    Add access token

---
+++ Controllers/auth/refreshController.js
@@ -0,0 +1,56 @@
+import Joi from "joi";
+import { REFRESH_SECRET } from "../../Config";
+import { RefreshToken, User } from "../../models";
+import CustomErrorHandler from "../../services/CustomErrorHandler";
+import JwtService from "../../services/JwtService";
+
+const refreshController = {
+    async refresh(req, res, next) {
+        // validation
+        const refreshSchema = Joi.object({
+            refresh_token: Joi.string().required()
+        });
+
+        const { error } = refreshSchema.validate(req.body);
+
+        if (error) {
+            return next(error);
+        }
+
+        // database
+        let refreshtoken
+        try {
+            refreshtoken = await RefreshToken.findOne({ token: req.body.refresh_token })
+            if (!refreshtoken) {
+                return next(new CustomErrorHandler.unAuthorized('Invalid refreh token'))
+            }
+
+            let userId;
+            try {
+                const { _id } = await JwtService.verify(refreshtoken.token, REFRESH_SECRET);
+                userId = _id;
+
+            } catch (err) {
+                return next(new CustomErrorHandler.unAuthorized('Invalid refreh token'))
+            }
+
+            const user = User.findOne({ _id: userId });
+            if (!user) {
+                return next(new CustomErrorHandler.unAuthorized('No user fond!'))
+            }
+
+            // Token
+            const access_token = JwtService.sign({ _id: user._id, role: user.role })
+            const refresh_token = JwtService.sign({ _id: user._id, role: user.role }, '1y', REFRESH_SECRET)
+            // database whitelist
+
+            await RefreshToken.create({ token: refresh_token })
+            res.json({ access_token, refresh_token });
+
+        } catch (err) {
+            return next(new Error('Something went wrong' + err.massage));
+        }
+    }
+};
+
+export default refreshController;
--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { loginController, registerController, userController } from '../Controllers';
+import { loginController, registerController, userController , refreshController} from '../Controllers';
 import auth from '../middlewares/auth';
 
 router.post('/register', registerController.register)
 router.post('/login', loginController.login)
 router.get('/me', auth, userController.me);
+router.post('/refresh', refreshController.refresh);
 
 export default router;
