1d812a99d382943add0a2a4f4b2c6f59404b5f7c
M Anas Latif <m.anaslatif.com@gmail.com>
2022-10-11 00:34:29 +0500

    add log out

--- Controllers/auth/loginController.js
+++ Controllers/auth/loginController.js
@@ -39,10 +39,31 @@ const loginController = {
             // database whitelist
 
             await RefreshToken.create({ token: refresh_token })
-            res.json({ access_token , refresh_token });
+            res.json({ access_token, refresh_token });
         } catch (err) {
             return next(err)
         }
+    },
+
+    async logout(req, res, next) {
+        // validation
+        const refreshSchema = Joi.object({
+            refresh_token: Joi.string().required()
+        });
+
+        const { error } = refreshSchema.validate(req.body);
+
+        if (error) {
+            return next(error);
+        }
+
+        try {
+            // dalete on database
+            await RefreshToken.deleteOne({ token: req.body.refresh_token })
+        } catch (err) {
+            return next(new Error('Something went wrong in database'))
+        }
+        res.json({status : 1});
     }
 }
 
